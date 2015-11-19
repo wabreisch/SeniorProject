d10f6f094b552f1d9d6773f9d195afb8608e2cc2
wabreisch <wabreisch@quinnipiac.edu>
2015-11-19 13:25:25 -0500

    Adds router.js

---
+++ MeteorApp/lib/router.js
@@ -0,0 +1,8 @@
+Router.configure({ // define some default templates
+  notFoundTemplate: 'notFound',
+  layoutTemplate: 'layout'
+});
+
+Router.route('/onoff/', {name: 'onoff'});
+
+Router.route('/portal/', {name: 'portal'});
