44805056dd4a4d103b525252ea264f5c246f4941
wabreisch <wabreisch@quinnipiac.edu>
2015-11-19 14:22:20 -0500

    Merge 2 portals into one portal

--- MeteorApp/lib/router.js
+++ MeteorApp/lib/router.js
@@ -4,5 +4,4 @@ Router.configure({ // define some default templates
 });
 
 Router.route('/onoff/', {name: 'onoff'});
-Router.route('/instructorportal/', {name: 'instructorPortal'});
-Router.route('/studentportal/', {name: 'studentPortal'});
+Router.route('/portal/', {name: 'portal'});
